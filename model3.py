fd3ea970b41a63d8eaa54ff75a2a1f4112f4d157
Prakhar Pande <pandeprakhar@ymail.com>
2019-01-05 16:16:16 +0530

    model 3 modified

--- model3.py
+++ model3.py
@@ -5,24 +5,29 @@ import pandas as pd
 from sklearn import preprocessing
 #sigmoid function
 def sigmoid(z):
-    sig =  1./(1 + np.exp(-z))
+    sig =  1/(1 + np.exp(-z))
     sig = np.clip(sig,1e-6,1)
     return sig
 #costfunction
 def costfunc(X,Y,Theta):
-    h_theta = np.zeros((len(X),1))
-    k = X.dot(Theta)
+    k = np.dot(X,Theta)
     h_theta = sigmoid((k))
-    M = len(X)
-    cost = (1/M)*(-np.transpose(Y).dot(np.log10(h_theta))-np.transpose(1-Y).dot(np.log10(1-h_theta)))
+    cost = (1/len(Y))*(np.dot(-np.transpose(Y) , np.log10(h_theta)) + np.dot(np.transpose(1-Y) , np.log10(1-h_theta)))
+    cost = np.clip(cost,-100000,100000)
     return cost
 #gradient function
 def gradfunc(X,Y,Theta):
-    m = len(Y)
-    k = X.dot(Theta)
+    k = np.dot(X,Theta)
     h_theta = sigmoid((k))
-    grad = (np.dot(X.transpose(),np.subtract((h_theta),Y.reshape(8000,1))))/m
+    Y = np.reshape(Y,(8000,1))
+    grad = np.dot(np.transpose(h_theta - Y),X)/len(Y)
+    grad = np.transpose(grad)
     return grad
+#minimizing cost
+def gradientdescent(X,Y,Theta,alpha,maxiter):
+    for i in range(0,maxiter):
+        Theta = Theta - alpha*gradfunc(X,Y,Theta)
+    return Theta
 #Model Begins
 print("This is a machine learning model..........")
 print("this model works on the algorithm of logistic regression..........")
@@ -36,6 +41,8 @@ X_val = data.iloc[8000:10000,1:13].values
 Y_val = data.iloc[8000:10000,0].values
 m = len(Y_train)
 X_train = np.hstack([np.ones((m,1)),X_train])
+X_train = preprocessing.normalize(X_train)
+print(X_train)
 X_val = np.hstack([np.ones((len(Y_val),1)),X_val])
 #initializing parameters
 Theta = np.random.rand(13,1)
@@ -43,4 +50,10 @@ Theta = np.random.rand(13,1)
 cost = costfunc(X_train,Y_train,Theta)
 grad = gradfunc(X_train,Y_train,Theta)
 print(cost)
-print(grad)
+#print(grad)
+#Running gradient descent
+alpha = 0.00001
+maxiter = 5000
+Theta = gradientdescent(X_train,Y_train,Theta,alpha,maxiter)
+print("The optimal value of Theta is:")
+print(Theta)
